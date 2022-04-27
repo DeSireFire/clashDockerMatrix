6ed049e86965f3bd75364653374377eeeece4384
RaXianch <1025212779@qq.com>
2022-04-27 16:06:46 +0800

    base update

--- clash_yaml2docker.py
+++ clash_yaml2docker.py
@@ -50,13 +50,14 @@ def getDockerName():
                 temp[line.split()[2]] = line.split()[0]
     return temp
 
-def resultDocker_comm(new_comm, old_comm):
+def resultDocker_comm(new_comm, old_comm, dockered):
     """
     整合处理各个容器需要运行的命令
     有变化的docker容器重启，
     没出现过的docker批量运行
     :param new_comm: 启动的新容器的命令
     :param old_comm: 重启更新的旧容器的命令
+    :param dockered: docker 已启动名单和容器编号映射关系
     :return: 整合后的需要运行的命令
     """
     result_comms = {}
@@ -64,17 +65,18 @@ def resultDocker_comm(new_comm, old_comm):
         # 比对端口占用，端口相同但是文件名不同就干掉容器，使用新的
         for k, v in old_comm.items():                           # {'clash—7890-xx': 'docker restart a4e0c2bc78e4'}
             for m, n in new_comm.items():                       # {'clash—7910-xx': "docker run -d --name 'clash-7910-1649399371900' --restart always -p 7910:7890 -p 7911:7891 -p 9100:9090 -v /home/ubuntu/software/clash/configs/1649399371900.yml:/root/.config/clash/config.yaml dreamacro/clash"}
-                k_temp = k.split("-")[1]
-                m_temp = m.split("-")[1]
-                if k_temp == m_temp:
-                    if k == m:
+                k_temp = k.split("-")[1]    # 获取旧docker所有占用的端口号
+                m_temp = m.split("-")[1]    # 获取新docker将要占用的端口号
+                if k_temp == m_temp:    # 端口占用判断
+                    if k == m:  # 容器名称完全相同
                         print(f'{k} 容器无变化仅更新配置，直接重启')
                         result_comms[k] = [v]   # ['docker restart a4e0c2bc78e4']
-                    else:
+                    else:       # 容器名称不完全相同，说明端口有变化
                         print(f'{k} 容器有变化且存在端口占用！')
+                        docker_id = dockered.get(k)
                         result_comms[k] = [
-                            f"docker stop {k}", # 关闭旧容器
-                            f"docker container rm {k}",  # 删除容器实例
+                            f"docker stop {docker_id}", # 关闭旧容器
+                            f"docker container rm {docker_id}",  # 删除容器实例
                             n,  # docker run 一个新容器
                         ]
                 else:
@@ -84,6 +86,8 @@ def resultDocker_comm(new_comm, old_comm):
         # 如果该服务器上完全没有运行clash容器，old_comm为空字典
         for m, n in new_comm.items():
             print(f'{m} 为全新容器!')
+            k_temp = m.split("-")[1]
+            print(k_temp)
             result_comms[m] = [n]  # ["docker run -d --name 'clash-7910-1649399371900' ......"]
 
     return result_comms
@@ -115,7 +119,6 @@ def get_yaml_list(file_dir='configs'):
     return files
 
 
-
 if __name__ == '__main__':
     # 获取配置文件
     yaml_list = get_yaml_list(os.path.join(base_path, "configs"))
@@ -136,18 +139,23 @@ if __name__ == '__main__':
     for k,v in dockered.items():
         dockered_comms[k] = f"docker restart {v}"
 
-    print(docker_comms)
-    print("*" * 50)
-    print(dockered_comms)
+    # print(docker_comms)
+    # print("*" * 50)
+    # print(dockered_comms)
 
     # 整合命令
-    docker_comms_result = resultDocker_comm(docker_comms,dockered_comms)
+    docker_comms_result = resultDocker_comm(docker_comms,dockered_comms,dockered)
 
     # 批量启动clash docker
     docker_runs = {}
     for a,b in docker_comms_result.items():
         print(f'执行容器{a},命令：{b},')
-        docker_runs[a] = [cmdRuner(i) for i in b]
+        temp_consoles = []
+        for c in b:
+            temp_consoles.append(cmdRuner(c))
+            time.sleep(3)
+        docker_runs[a] = temp_consoles
+        # docker_runs[a] = [cmdRuner(i) for i in b]
         print(f"等待执行结束..")
         time.sleep(5)
 
